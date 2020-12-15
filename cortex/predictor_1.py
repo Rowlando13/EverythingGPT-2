6ec1fcbae285b90e4c43585cdf1ede789f879336
Edward <edward.girling@gmail.com>
2020-12-15 10:20:35 -0800

    updated for running onnx models

--- cortex/predictor_1.py
+++ cortex/predictor_1.py
@@ -1,12 +1,14 @@
-# this is an example for cortex release 0.19 and may not deploy correctly on other releases of cortex
+# cortex version 0.19 and may not deploy correctly on other releases of cortex
 import torch
 import boto3
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import nltk
 import os
 import json
+import cstm_generate
 
 def s3_download(cortex_base, modelpath, s3bucket, s3):
+    ''' downloads folder and all subfiles from s3 folder'''
     #making path in cortex directory
     os.makedirs(os.path.join(cortex_base, modelpath), exist_ok=True)
     #getting response object for s3 folder
@@ -16,22 +18,25 @@ def s3_download(cortex_base, modelpath, s3bucket, s3):
     for s3_obj in response["Contents"]:
         s3_obj_key = s3_obj["Key"]
         s3.download_file(s3bucket, s3_obj_key, os.path.join(cortex_base, s3_obj_key))
+        #print so can see what is downloaded in the logs
         print("downloaded s3://{}/{} to {}/{}. \n".format(s3bucket, s3_obj_key, cortex_base, s3_obj_key), end = '')
 
 def text_cleanup(text_gen, list_gen, mode):
-    """Take text generated, Text generated as list of sentences and mode, returns string of finalized sentence """
-    #generates only 1 sentence
+    """Take text generated, Text generated as list of sentences and mode, returns string of finalized sentence. Sorts generated text in different
+    cases"""
+    #generated only 1 sentence
     if len(list_gen)<=1:
         clean_text_gen=text_gen
     else:
-        #getting truncating the generating text for each mode
-        # first if else for case where completion[0] == punctuation, crude fix
+        #where completion[0] == punctuation
         if text_gen[0] not in [".", "?", "!"]:
+            #getting final text for each mode
             if mode=='s-completion':
                 clean_text_gen=list_gen[0]
-            #not perfect, if give complete sentence, then gives them two sentences
+            #if give complete sentence, then gives them two sentences
             elif mode=="s-completion+":
                 clean_text_gen=list_gen[0]+list_gen[1]
+            #paragraph completion
             else:
                 line_break=text_gen.find("\n")
                 if line_break !=-1:
@@ -41,7 +46,7 @@ def text_cleanup(text_gen, list_gen, mode):
         else:
             if mode=='s-completion':
                 clean_text_gen=list_gen[0]+list_gen[1]
-            #not perfect, if give complete sentence, then gives them two sentences
+            #if give complete sentence, then gives them two sentences
             elif mode=="s-completion+":
                 #if not enough sentences
                 if len(list_gen)<=2:
@@ -59,43 +64,45 @@ def text_cleanup(text_gen, list_gen, mode):
 
 class PythonPredictor:
     def __init__(self, config):
+        '''Runs once when deploying'''
+
+        #sent_tokenize not included so have to download
+        nltk.download("punkt")
+
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         print(f"using device: {self.device}")
         #credentials
         aws_access_key_id="null"
         aws_secret_access_key="null"
-        cortex_base1="tmp1"
+        cortex_base1="tokenizer"
         modelpath1='gpt2_124m_base'
-        cortex_base2="tmp2"
+        cortex_base2="models"
         modelpath2='gpt2_124m_base'
         s3bucket='deranged-parrot-models'
 
         #creating client and getting list of objects
         s3 = boto3.client("s3", aws_access_key_id=aws_access_key_id, \
                   aws_secret_access_key=aws_secret_access_key)
-        #downloading model1
-        s3_download(cortex_base=cortex_base1, modelpath=modelpath1, \
-        s3bucket=s3bucket, s3=s3)
-        #testing
-        #s3_download(cortex_base=cortex_base2, modelpath=modelpath2, \
-        #s3bucket=s3bucket, s3=s3)
+        #downloading motokenizer and model
+        s3_download(cortex_base1, modelpath1, s3bucket, s3)
+        s3_download(cortex_base2, modelpath2, s3bucket, s3)
 
         self.tokenizer = GPT2Tokenizer.from_pretrained(os.path.join(cortex_base1, modelpath1)).to(self.device)
-        #use_cache speeds up decoding by storing and repassing hidden states. Possible out of memory issue.
-        self.model1 = GPT2LMHeadModel.from_pretrained(\
-        os.path.join(cortex_base1, modelpath1), use_cache=True).to(self.device)
-        #testing
-        #self.model2= GPT2LMHeadModel.from_pretrained(\
-        #os.path.join(cortex_base2, modelpath2), use_cache=True).to(self.device)
-        #nltk sentence tokenizer is not in basic module
-        nltk.download("punkt")
+
+        self.model1 = #(\
+        os.path.join(cortex_base1, modelpath1), use_cache=True)
+
+        # if using torch model then uncomment this and 1 other spot
+        #self.model1 = GPT2LMHeadModel.from_pretrained(\
+        #os.path.join(cortex_base1, modelpath1), use_cache=True).to(self.device)
+
 
     def predict(self, payload):
-        #Need to add code so python formatted text converts to multiline string
+        '''Is called each time you make a call to the predictor '''
         #for data logging
         print(payload)
 
-        #prevent accidently accepting True or False
+        #prevent accidently someone trying to overload our parsing by passing logical statements
         api_key=str(payload["api_key"])
 
         #verifying key
@@ -111,7 +118,7 @@ class PythonPredictor:
             else:
                 gen_len=450
 
-            #storing text for readibility and effeciency
+            #storing text for readibility and efficiency
             truncated=False
             raw_text=payload["text"]
 
@@ -123,7 +130,7 @@ class PythonPredictor:
             #if input is too long, then shortening so program will still run
             if input_length+gen_len > 1023:
                 text = self.tokeizer.decode(tokens[-573:])
-                tokens = self.tokenizer.encode(text, return_tensors="pt").to(self.device)
+                tokens = tokens[-573:]
                 input_length = 573
                 truncated=True
             #do nothing to text, tokens
@@ -131,9 +138,22 @@ class PythonPredictor:
                 text = raw_text
 
             if payload["pred_name"]=="model1":
-                prediction = self.model1.generate(tokens, min_length=input_length+gen_len-5, max_length=input_length+gen_len, temperature=payload["temperature"], repetition_penalty=payload["repetition_penalty"], top_k=payload["top_k"], top_p=payload["top_p"], do_sample=True, num_return_sequences=payload["batch_size"])
-            else :
-                prediction = self.model2.generate(tokens, min_length=input_length+gen_len-5, max_length=input_length+gen_len, temperature=payload["temperature"], repetition_penalty=payload["repetition_penalty"], top_k=payload["top_k"], top_p=payload["top_p"], do_sample=True, num_return_sequences=payload["batch_size"])
+                prediction = cstm_generate.generate_no_beam_search(self.model1, tokens,
+                input_length,
+                input_length+gen_len,
+                input_length+gen_len-5,
+                payload["temperature"],
+                payload["top_k"],
+                payload["top_p"],
+                payload["repetition_penalty"],
+                payload["batch_size"],
+                sess_type="onnx"
+                )
+
+
+            #uncomment if using torch model
+            #if payload["pred_name"]=="model1":
+            #    prediction = self.model1.generate(tokens, min_length=input_length+gen_len-5, max_length=input_length+gen_len, temperature=payload["temperature"], repetition_penalty=payload["repetition_penalty"], top_k=payload["top_k"], top_p=payload["top_p"], do_sample=True, num_return_sequences=payload["batch_size"])
 
             #creating final answer
             final_answer={"text":{}, "truncated":truncated}
